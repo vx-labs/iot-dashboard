2eb4f2a830e515647f424052f73c59e92591aae8
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-05-30 11:54:19 +0200

    create appdrawer component

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-if="authenticated" app permanent expand-on-hover>
-      <v-list two-line>
-        <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <v-img :src="profile.picture"></v-img>
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title>{{ profile.given_name}} {{ profile.family_name}}</v-list-item-title>
-            <v-list-item-subtitle>{{ profile.email}}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
-
-      <v-list nav dense v-if="authenticated">
-        <v-list-item link to="/">
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Home page</v-list-item-title>
-        </v-list-item>
-        <v-list-item link to="/devices/">
-          <v-list-item-icon>
-            <v-icon>mdi-devices</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>My IoT Devices</v-list-item-title>
-        </v-list-item>
-      </v-list>
-      <template v-slot:append>
-        <div class="pa-2">
-          <AuthButton></AuthButton>
-        </div>
-      </template>
-    </v-navigation-drawer>
+    <AppDrawer></AppDrawer>
     <v-app-bar app color="white" dense>
       <v-toolbar-title>🐝 Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
@@ -50,23 +16,16 @@
 </template>
 
 <script lang="ts">
-import AuthButton from "@/components/AuthButton.vue";
+import AppDrawer from "@/components/AppDrawer.vue";
 import Vue from 'vue';
-import { mapGetters, mapActions } from 'vuex'
+import { mapActions } from 'vuex'
 export default Vue.extend({
   name: 'App',
-  components: { AuthButton },
+  components: { AppDrawer },
   methods: {
     ...mapActions(["handleAuthentication"])
   },
-  computed: {
-    ...mapGetters(["authenticated", "profile"])
-  },
-  data: () => ({
-    items: [{ title: "Devices", "icon": "bug" }]
-  }),
   mounted() {
-
     this.handleAuthentication().then(() => {
       if (
         window.location.search.includes("code=")) {
